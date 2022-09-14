cac5a3c950d3e1093e8ea5e1472260d5e7154d38
JCJ05 <julio_aguero@usmp.pe>
2022-09-13 20:18:20 -0500

    Esquema del Proyecto

---
+++ spring-app-core/src/main/java/com/app/bancario/springappcore/SpringAppCoreApplication.java
@@ -0,0 +1,13 @@
+package com.app.bancario.springappcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppCoreApplication.class, args);
+	}
+
+}
---
+++ spring-app-core/src/test/java/com/app/bancario/springappcore/SpringAppCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bancario.springappcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
