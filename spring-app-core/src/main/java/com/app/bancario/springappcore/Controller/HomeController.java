2246f9ec465ec7900e589e037b36a3eca4a5740c
JCJ05 <julio_aguero@usmp.pe>
2022-09-16 19:51:38 -0500

    Arreglando error dr ruta y validaciones

--- spring-app-core/src/main/java/com/app/bancario/springappcore/Controller/HomeController.java
+++ spring-app-core/src/main/java/com/app/bancario/springappcore/Controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.app.bancario.springappcore.controller;
+package com.app.bancario.springappcore.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
