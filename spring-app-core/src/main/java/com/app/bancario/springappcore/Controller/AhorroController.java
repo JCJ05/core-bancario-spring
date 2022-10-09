d275743686fe5f6c80100f51a5325a5fbb0fed9e
JCJ05 <julitoaguerorengifo@hotmail.com>
2022-10-08 20:14:21 -0500

    Mensajes de alerta, sweetlaert , animate css, icon

--- spring-app-core/src/main/java/com/app/bancario/springappcore/Controller/AhorroController.java
+++ spring-app-core/src/main/java/com/app/bancario/springappcore/Controller/AhorroController.java
@@ -1,15 +1,25 @@
 package com.app.bancario.springappcore.Controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/ahorro")
 public class AhorroController {
     @GetMapping(path = {"/", "", "/index"})
-    public String index(Model model){
+    public String index(Model model , Principal principal , RedirectAttributes attributes){
+        
+        if(principal == null){
+
+            attributes.addFlashAttribute("autorizacion", "Para poder solicitar la apertura de una cuenta de ahorro necesita ser cliente de nuestro banco asi que por favor inicie sesion o cree una nueva cuenta");
+            return "redirect:/usuario/login";
+       }
+
         return "ahorro/form_ahorros";
     }
 }
--- spring-app-core/src/main/java/com/app/bancario/springappcore/Controller/PrestamoController.java
+++ spring-app-core/src/main/java/com/app/bancario/springappcore/Controller/PrestamoController.java
@@ -1,21 +1,37 @@
 package com.app.bancario.springappcore.Controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/prestamo")
 public class PrestamoController {
     
     @GetMapping(path = {"/", "", "/index"})
-    public String index(Model model){
+    public String index(RedirectAttributes attributes, Principal principal){
+       
+      if(principal == null){
+
+        attributes.addFlashAttribute("autorizacion", "Para poder solicitar un prestamo necesita ser cliente de nuestro banco asi que por favor inicie sesion o cree una nueva cuenta");
+           return "redirect:/usuario/login";
+      }
+
         return "prestamo/index";
     }
 
     @GetMapping(path = {"solicitar"})
-    public String solicitarPrestamo(Model model){
+    public String solicitarPrestamo(RedirectAttributes attributes , Principal principal){
+
+        if(principal == null){
+
+            attributes.addFlashAttribute("autorizacion", "Para poder solicitar un prestamo necesita ser cliente de nuestro banco asi que por favor inicie sesion o cree una nueva cuenta");
+            return "redirect:/usuario/login";
+       }
+ 
         return "prestamo/solicitud";
     }
 
